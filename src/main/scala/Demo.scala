9b49a267d78ff2435be32fdc4c0238f4fe6262c7
shaik <mougamadoushaik2@gmail.com>
2022-03-22 20:52:51 +0100

    fixs

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,7 +1,7 @@
 import breeze.plot._
 import breeze.linalg._
 import java.util
-
+import scala.util.Random
 
 object Demo {
 
@@ -11,29 +11,27 @@ object Demo {
     val kMeans = new KMeans(dataLoader.load("iris.data"), 3)
     kMeans.cluster()
     kMeans.printClusters()
+
     val centroidsList = kMeans.getCentroids
     val pointsList = kMeans.getPoints
 
+    val rand = scala.util.Random
     val figure = Figure("K-means Iris")
     val p = figure.subplot(0)
     p.title = "Sepal datas"
 
     var i= 0
-    while (i < pointsList.get(0).size) {
-      p+=plot(List(pointsList.get(0).get(i).getX), List(pointsList.get(0).get(i).getY), '.', "[0,185,39]", "Cluster 1")
-      i=i+1
-    }
-
-    i=0
-    while (i < pointsList.get(1).size) {
-      p+=plot(List(pointsList.get(1).get(i).getX), List(pointsList.get(1).get(i).getY), '.', "[255,0,0]", "Cluster 2")
-      i=i+1
-    }
-
-    i=0
-    while (i < pointsList.get(2).size) {
-      p+=plot(List(pointsList.get(2).get(i).getX), List(pointsList.get(2).get(i).getY), '.', "[0,201,255]", "Cluster 3")
-      i=i+1
+    var j= 0
+    var randColor = 0
+    val colorsArray = List("[0,255,39]","[0,255,251]","[220,0,255]","[255,0,108]","[106,45,245]","[245,112,45]","[20,159,255]")
+    while (i < pointsList.size()) {
+      j=0
+      randColor = rand.nextInt(colorsArray.size)
+      while (j < pointsList.get(i).size()) {
+        p+=plot(List(pointsList.get(i).get(j).getX), List(pointsList.get(i).get(j).getY), '.', colorsArray(randColor), "Cluster")
+        j+=1
+      }
+      i+=1
     }
 
     i=0
@@ -41,6 +39,7 @@ object Demo {
       p+=plot(List(centroidsList.get(i).getX), List(centroidsList.get(i).getY), '+', "[0,0,0]")
       i=i+1
     }
+
     p.xlabel = "Sepal.Length"
     p.ylabel = "Sepal.Width"
     figure.saveas("test.png")
